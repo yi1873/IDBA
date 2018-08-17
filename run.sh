1eb4ca7e3c387b5014f7853fedb0cab74435230c
Xiangzhi Liang <xiang_zhi_@126.com>
2018-08-17 16:51:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 docker run --rm -v /path:/path \
-idba_ud:v1.0.0 \
-/bin/bash /idba/idba_ud.assembly.sh -s prefix \
--x /path/test.pair.1.fq.gz \
--p /path/test.pair.2.fq.gz \
--o /path/idba \
--m 120 -i 40 -e 20 -t 20
+       idba_ud:v1.0.0 \
+       /bin/bash /idba/idba_ud.assembly.sh -s prefix \
+       -x /path/test.pair.1.fq.gz \
+       -p /path/test.pair.2.fq.gz \
+       -o /path/idba \
+       -m 120 -i 40 -e 20 -t 20
